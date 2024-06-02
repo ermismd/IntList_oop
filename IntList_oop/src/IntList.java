ad58bd58b80c21d33e411590619e195869708430
ermismd <ermismdelopoulos@outlook.com>
2024-06-01 19:23:38 -0500

    data abstraction and defined bodies part1

--- IntList_oop/src/IntList.java
+++ IntList_oop/src/IntList.java
@@ -1,4 +1,26 @@
 
 public class IntList {
 
+private int[] sequence;
+
+public IntList(int[] sequence) {
+	this.sequence=sequence.clone();
+	
+}
+	
+public int getLength() {
+	return sequence.length;
+}
+public int[] getSequence() {
+	return sequence.clone();
+}
+
+
+public void addValue(int newValue) {
+	int index=getLength();
+	sequence[index+1]=newValue;
+}
+
+	
+	
 }
